8a449f5ab19bbef2f8a383c02a8d8033a046970e
Zion <bone2057@gmail.com>
2019-04-24 16:41:06 +0800

    refractor webhook method

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -47,26 +47,58 @@ class KamigoController < ApplicationController
   end
 
   def webhook
-    # Line Bot API 物件初始化
-    client = Line::Bot::Client.new{|config|
+
+    # 設定回覆文字
+    reply_text = keyword_reply(receive_text)
+
+    # 傳送訊息到 line
+    response = reply_to_line(reply_text)
+
+    # 回應 200
+    head :ok
+    
+  end
+  
+  # Line Bot API 物件初始化
+  def line
+    # return @line unless @line.nil?  
+    @line ||= Line::Bot::Client.new{|config|
       config.channel_secret = '57d1d732a9dd3f02aeee87b3c882a481'
       config.channel_token = 'CvaRq33hjRaTSFurt8t24NPakKRZ7oVLs04nhOTeZydD3CCMpZtTAHcKPEgBoAtnHwt5QuO6i8oxKh6xXwvli+RqOGILUd4qY2AcOLgzbyPb5H7HZdrjn6pAQdYlFcaozRMWUtX9yFoDTXLM6CgEfAdB04t89/1O/w1cDnyilFU='
     }
-    # 取得 reply token
+  end
+
+  # 傳送訊息到 line
+  def reply_to_line(message)
+    return nil if reply_text.nil?
+
+    # 取得reply token
     reply_token = params['events'][0]['replyToken']
 
-    p "======這裡是 reply_token ======"
-    p reply_token 
-    p "============"
-    
     # 設定回覆訊息
     message = {
       type: 'text',
-      text: '旺～旺～'
+      text: reply_text
     }
+
     # 傳送訊息
-    response = client.reply_message(reply_token, message)
-    head :ok
+    line.reply_message(reply_token, message)
+
+  end
+
+  # 取得對方說的話
+  def receive_text
+    params['events'][0]['message']['text']
+    message['text'] unless message.nil?
+  end
+
+  def keyword_reply(received_text)
+    # 學習紀錄表
+    keyword_mapping = {
+      'QQ' => '神曲支援：https://www.youtube.com/watch?v=T0LfHEwEXXw&feature=youtu.be&t=1m13s',
+      '我難過' => '神曲支援：https://www.youtube.com/watch?v=T0LfHEwEXXw&feature=youtu.be&t=1m13s'
+    }
+    keyword_mapping[received_text]
   end
 
 end
