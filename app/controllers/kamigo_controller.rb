deae2dd24ca1b72d4ec41611200aff0aa8ede237
Zion <bone2057@gmail.com>
2019-04-27 23:02:52 +0800

    add column channel_id to keyword_mappings

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -48,10 +48,10 @@ class KamigoController < ApplicationController
 
   def webhook
     # 學說話
-    reply_text = learn(received_text)
+    reply_text = learn(channel_id, received_text)
 
     # 設定回覆文字
-    reply_text = keyword_reply(received_text) if reply_text.nil?
+    reply_text = keyword_reply(channel_id, received_text) if reply_text.nil?
 
     # 重複
     reply_text = echo2(channel_id, received_text) if reply_text.nil?
@@ -68,7 +68,7 @@ class KamigoController < ApplicationController
     
   end
 
-  def learn(received_text)
+  def learn(channel_id, received_text)
     #如果開頭不是 卡米狗學說話; 就跳出
     return nil unless received_text[0..6] == '卡米狗學說話;'
 
@@ -80,7 +80,7 @@ class KamigoController < ApplicationController
 
     keyword = received_text[0..semicolon_index-1]
     message = received_text[semicolon_index+1..-1]
-    KeywordMapping.create(keyword: keyword, message: message)
+    KeywordMapping.create(channel_id: channel_id, keyword: keyword, message: message)
     'got it ~'
   end
   
@@ -117,9 +117,12 @@ class KamigoController < ApplicationController
     message['text'] unless message.nil?
   end
 
-  def keyword_reply(received_text)
+  def keyword_reply(channel_id, received_text)
     # 學習紀錄表
+    message = KeywordMapping.where(channel_id: channel_id, keyword: received_text).last&.message
+    return message unless message.nil?
     KeywordMapping.where(keyword: received_text).last&.message
+    
   end
 
   def channel_id
